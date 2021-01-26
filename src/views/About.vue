a4c331292077e09c1f99f4dc299d8e4928b7d212
gzh123 <123456@qq.com>
2021-01-26 15:57:32 +0800

    VUE的计算属性和监听方法

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,53 @@
 <template>
-  <div class="about">
-    <div id="app">
-		<h1>计数器</h1>
-		<button @click="al">-</button>
-		<span>{{num}}</span>
-		<button @click="li">+</button>
+	<div class="about">
+		<div id="app">
+			<h1>计数器</h1>
+			<button @click="onlick">-</button>
+			<span>{{num}}</span>
+			<button @click="twolick">+</button>
+			<br>
+			<span>{{ comput1() }}</span>
+			<span>{{ comput2 }}</span>
+			<br>
+			<h1>计数器2</h1>
+			<button @click="num2--">-</button>
+			<span>{{num2}}</span>
+			<button @click="num2++">+</button>
+		</div>
 	</div>
-  </div>	
 </template>
 <script>
-export default {
-	data(){
-		return{
-			num:0
-		}
-	},
-	methods:{
-		al(){
-			this.num--;
+	export default {
+		data() {
+			return {
+				num: 0,
+				num2: 0,
+			}
 		},
-		li(){
-			this.num++;
-		}
+		methods: {
+			comput1() {
+				console.log("普通方法执行")
+				return "普通方法"
+			},
+			onlick() {
+				this.num--;
+			},
+			twolick() {
+				this.num++;
+			}
+		},
+		computed: {
+			// 计算属性
+			comput2() {
+				console.log("计算方法执行")
+				return "计算方法" + this.num+this.num2
+			}
+		},
+		watch: {
+			// 方法监听
+			num(newValue,oldValue){
+				console.log("监听到num变化",newValue,oldValue)
+			}
+		},
 	}
-}
 </script>
